39f659474ae15444787f54d70ad189c64a2f53d4
DevExpressExampleBot <fakeEmail>
2022-02-02 12:43:28 +0300

    Source auto update [skip ci]

--- VB/RemoveSpecificMarkupAnnotations/Program.vb
+++ VB/RemoveSpecificMarkupAnnotations/Program.vb
@@ -1,25 +1,21 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 Imports System.Linq
 
 Namespace RemoveSpecificMarkupAnnotations
 
     Friend Class Program
-        Shared Sub Main(ByVal args() As String)
 
+        Shared Sub Main(ByVal args As String())
             ' Create a PDF Document Processor.
-            Using processor As New PdfDocumentProcessor()
-
+            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Load a document.
                 processor.LoadDocument("..\..\Document.pdf")
-
                 For i As Integer = 0 To processor.Document.Pages.Count
-
                     ' Remove Bill Smith's markup annotations from a page.
                     processor.DeleteMarkupAnnotations(processor.GetMarkupAnnotationData(i).Where(Function(annotation) annotation.Author.Contains("Bill Smith")))
-
                     ' Save the result document.
                     processor.SaveDocument("..\..\Result.pdf")
-                Next i
+                Next
             End Using
         End Sub
     End Class
